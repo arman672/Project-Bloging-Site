d17f6ea23067db25fa66823d5b946a4fdc7c5803
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-06-21 14:57:57 +0530

    Controller_created

--- controllers/author.controllers.js
+++ controllers/author.controllers.js
@@ -1,4 +1,4 @@
-const Author = require('../models/author.model');
+const Author = require('./models/author.model');
 
 
 //Add author Router handler
@@ -11,6 +11,7 @@ const addAuthor = async (req,res) => {
     //Validation for data is data is present inside body or not
     if(!data.fname) return res.status(400).send({ status: false, msg: "First Name is required" });
     if(!data.lname) return res.status(400).send({ status: false, msg: "Last Name is required" });
+    if(!data.body) return res.status(400).send({ status: false, msg: "Body is required" });
     if(!data.title) return res.status(400).send({ status: false, msg: "Title is required" });
     if(!data.email) return res.status(400).send({ status: false, msg: "Email is required" });
     if(!data.password) return res.status(400).send({ status: false, msg: "Password is required" });
